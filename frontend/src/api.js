be0b6522b7ad753af73759b9829b3d34788bf196
Ravi Chaudhary <6312ravi6312@gmail.com>
2025-12-02 22:30:39 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.VITE_API_URL + "/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 export const registerUser = async ({ fullName, email, password }) => {
